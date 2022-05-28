5bc2243b67f8eeb7f8d59a12d1fd8464aab98e19
leo <arias.leonardodev@gmal.com>
2022-05-27 21:28:23 -0400

    Working contact form

--- script.js
+++ script.js
@@ -17,4 +17,18 @@ const sr = ScrollReveal ({
 })
 
 sr.reveal('.home-content' ,{delay: 200, origin:'top'})
-sr.reveal('.about-text' ,{delay: 200, origin:'top'})
+sr.reveal('.about-text, .contact-text, .contact-form' ,{delay: 200, origin:'top'})
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
